002dd889ea99625c7746eeef477c0faecef06fe5
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-03-03 23:13:16 -0400

    Add ping endpoint for database connectivity

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import { pool } from './db.js'
 
 
 const app = express()
+
+app.get('/ping', async (req, res) => {
+  await pool.query('SELECT 1 + 1 as result')
+})
 
 app.get('/employees', (req, res) => res.send('obtain all employees'))
 app.post('/employees', (req, res) => res.send('add employees'))
