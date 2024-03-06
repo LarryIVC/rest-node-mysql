f91f39d3f78580c369d3a8b77ad09d09070c36b8
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-03-06 18:54:36 -0400

    Add getEmployeeById route to employees router

--- src/routes/employees.router.js
+++ src/routes/employees.router.js
@@ -1,11 +1,18 @@
 import { Router } from 'express';
-import { createEmployee, deleteEmployee, getEmployees, updateEmployee } from '../controllers/employees.controller.js';
+import {
+  createEmployee,
+  deleteEmployee,
+  getEmployees,
+  updateEmployee,
+  getEmployeeById
+} from '../controllers/employees.controller.js';
 
 const router = Router()
 
 router.get('/employees', getEmployees)
 router.post('/employees', createEmployee)
-router.put('/employees', updateEmployee) 
+router.put('/employees', updateEmployee)
 router.delete('/employees', deleteEmployee)
+router.get('/employees/:id', getEmployeeById)
 
 export default router
