e29081844861b59a8f86b4c9dc0cab4f2cff0b4b
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-03-06 16:57:44 -0400

    Add employee controller functions

---
+++ src/controllers/employees.controller.js
@@ -0,0 +1,7 @@
+ export const getEmployees = (req, res) => res.send('obtain all employees')
+
+ export const createEmployee = (req, res) => res.send('creating an employee')
+
+ export const updateEmployee = (req, res) => res.send('updating an employee')
+
+ export const deleteEmployee = (req, res) => res.send('deleting an employee') 
