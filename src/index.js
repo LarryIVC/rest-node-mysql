06d28b259a7dfff4e63a84d6ff08dcfa5e6aeecf
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-03-07 12:18:17 -0400

    Refactor server setup in index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
-import express from 'express'
-import employeesRoutes from './routes/employees.router.js'
-import indexRoutes from './routes/index.routes.js'
+import app from './app.js'
+import { PORT } from './config.js'
 
-const app = express()
+app.listen(PORT)
 
-app.use(express.json())
-
-app.use(indexRoutes)
-app.use('/api/v1', employeesRoutes)
-
-app.use((req, res, next) => {
-  res.status(404).json({ message: 'Endpoint not found' })
-})
-
-app.listen(3000)
-
-console.log('Server on port', 3000)
+console.log('Server on port', PORT)
