84a5a934102f61766470a8ff4b9abe058796b8b1
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-03-06 23:23:22 -0400

    Fix import statement in db.js

--- src/db.js
+++ src/db.js
@@ -1,9 +1,9 @@
-import { createPool } from "mysql2/promise";
+import { createPool } from 'mysql2/promise'
 
 export const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "Seguridad123*",
-  database: "companydb",
-  port: 3306,
+  host: 'localhost',
+  user: 'root',
+  password: 'Seguridad123*',
+  database: 'companydb',
+  port: 3306
 })
