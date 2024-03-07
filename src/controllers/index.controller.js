b6e911df141c91dbda775f2dc7022c60d3841b18
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-03-06 23:23:39 -0400

    Fix SQL query in ping function

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,6 +1,5 @@
 import { pool } from '../db.js'
 export const ping = async (req, res) => {
-  const [result] = await pool.query('SELECT \'Pong\' as result')
+  const [result] = await pool.query('SELECT "Pong" as result')
   res.json(result[0].result)
-
 }
