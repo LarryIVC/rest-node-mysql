3fc2c9714d57878efd19cff8461787b4e830c4d6
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-03-06 23:58:10 -0400

    Add 404 endpoint handler

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ app.use(express.json())
 app.use(indexRoutes)
 app.use('/api/v1', employeesRoutes)
 
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Endpoint not found' })
+})
+
 app.listen(3000)
 
 console.log('Server on port', 3000)
